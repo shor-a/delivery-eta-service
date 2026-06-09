f723e4de30cfee296a2462a91868c58b06eaf48d
Fadli Ashari <ashari.attamimi@gmail.com>
2026-06-09 10:45:00 +0700

    Spring Boot entry point

---
+++ src/main/java/com/delivery/estimator/EstimatorApplication.java
@@ -0,0 +1,13 @@
+package com.delivery.estimator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstimatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstimatorApplication.class, args);
+	}
+
+}
